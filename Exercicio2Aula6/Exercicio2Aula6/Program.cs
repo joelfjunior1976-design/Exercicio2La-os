d7f55bae9cbe2535762a82f82eb65dbca0218b9e
Joel Antonio Ferreira Junior <joel.f.junior1976@gmail.com>
2025-12-01 21:33:45 -0300

    adicionando exercicios

---
+++ Exercicio2Aula6/Exercicio2Aula6/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Descrição:
+//Dado um array ordenado de inteiros (positivos e negativos), retorne um novo array com os quadrados de 
+//cada número, também ordenados.
+// Por que usar laços:
+// É necessário percorrer o array com um laço 
+//for ou 
+//while para calcular os quadrados.
+// Depois, pode-se ordenar o resultado ou usar uma abordagem de dois ponteiros com 
+//while 
+{
+    var numeros = new int[] {-5,-1,4,8,2,3 };
+    for (int i = 0; i < numeros.Length; i++)
+  
+        {
+            numeros[i] = numeros[i] * numeros[i];
+        }
+        Array.Sort(numeros);
+        Console.WriteLine("Array de quadrados ordenados:");
+        foreach (var n in numeros)
+        {
+            Console.WriteLine(n);
+        }
+ }
---
+++ Exercicio4Aula6/Exercicio4Aula6/Program.cs
@@ -0,0 +1,15 @@
+﻿{ 
+        char[] caracteres = { 'J', 'o', 'e', 'l' };
+
+               for (int i = 0; i < caracteres.Length / 2; i++)
+        {
+
+            int j = caracteres.Length - 1 - i;
+            char temp = caracteres[i];
+            caracteres[i] = caracteres[j];
+            caracteres[j] = temp;
+        }
+
+        string resultado = new string(caracteres);
+        Console.WriteLine($"String invertida: {resultado}");
+    }
